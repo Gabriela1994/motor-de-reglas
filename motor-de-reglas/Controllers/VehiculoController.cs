2e3b80a2b2d0de121400bcb33766f467c4daaae1
Gabriela <101139215+Gabriela1994@users.noreply.github.com>
2025-01-27 20:46:37 -0300

    Revert "Crear endpoint Dummy para probar el worker (#3)" (#5)
    This reverts commit 683cd507b34635a1738fe9c526902d39f62559ce.

--- motor-de-reglas/Controllers/VehiculoController.cs
+++ motor-de-reglas/Controllers/VehiculoController.cs
@@ -1,7 +1,6 @@
 ﻿using DataAccess.Models;
 using DataAccess.Context;
 using Microsoft.AspNetCore.Mvc;
-using motor_de_reglas.Clases;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -52,18 +51,6 @@ namespace motor_de_reglas.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-        }
-
-        [HttpPost("evaluar-infracciones")]
-        public void EvaluarInfracciones(List<EventoCamara> eventos)
-        {
-            foreach(var evento in eventos)
-            {
-
-                Console.WriteLine("Patente: {0}", evento.Patente);
-                Console.WriteLine("Velocidad: {0} km/hora", evento.Velocidad);
-                Console.WriteLine("Color de semaforo: {0}", evento.ColorSemaforo);
-            }
         }
     }
 }
