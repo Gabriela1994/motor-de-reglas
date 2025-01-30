4b8d108e2ebe3621c5bd7a496764877d438374ed
Gabriela <101139215+Gabriela1994@users.noreply.github.com>
2025-01-30 20:51:45 -0300

    Se agregaron tablas en la bd: infraccion, tipoInfraccion y estadoInfraccion (#6)

---
+++ AccessData/Models/TipoInfraccion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models
+{
+    public partial class TipoInfraccion
+    {
+        public TipoInfraccion()
+        {
+            Infracciones = new HashSet<Infraccion>();
+        }
+
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+
+        public virtual ICollection<Infraccion> Infracciones { get; set; }
+    }
+}
